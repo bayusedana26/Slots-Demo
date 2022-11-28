7b4c58a0eef5ac72c956672f9d23a09583c365ef
Bayu Sedana <bayusedana26@gmail.com>
2022-11-28 10:50:08 +0700

    Initial Commit

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Bayu Sedana on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
